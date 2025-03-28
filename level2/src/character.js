3ec02d8f72a560914c034c28bc474356b9af1ae1
shaun <shauny1122@gmail.com>
2025-03-28 19:27:33 +0200

    Update character model path to include /level2/ prefix

--- level2/src/character.js
+++ level2/src/character.js
@@ -48,7 +48,7 @@ export class Character {
 
     loadModel() {
         const loader = new GLTFLoader();
-        loader.load('/models/character/Astronaut.glb', (gltf) => {
+        loader.load('/level2/models/character/Astronaut.glb', (gltf) => {
             const model = gltf.scene;
             model.scale.set(1, 1, 1); // Back to original size
             this.group.add(model);
